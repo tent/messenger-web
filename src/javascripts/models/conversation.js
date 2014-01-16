1c0a534b01e77a6ebe645d96ff55242044167605
Jesse Stuart <jesse@jessestuart.ca>
2014-01-16 15:27:54 -0500

    Refactor Message and Conversation models

--- src/javascripts/models/conversation.js
+++ src/javascripts/models/conversation.js
@@ -8,22 +8,123 @@
 		cidMappingScope: ['id', 'entity'],
 
 		willInitialize: function () {
-			this.type = Messenger.config.POST_TYPES.CONVERSATION;
+			this.newMessage = null;
 		},
 
-		save: function () {
-			data = this.toJSON();
+		didInitialize: function () {
+			this.set('type', this.type || Messenger.config.POST_TYPES.CONVERSATION);
+		},
 
-			console.log('TODO: Save conversation', data);
-			// TODO trigger save:success(res, xhr)
-			// TODO trigger save:failure(res, xhr)
-			// TODO trigger save:complete(success, res, xhr)
+		setRecipients: function (entities) {
+			var mentions = [];
+			for (var i = 0, _len = entities.length; i < _len; i++) {
+				mentions.push({
+					entity: entities[i]
+				});
+			}
+			this.set('mentions', mentions);
+		},
+
+		setNewMessage: function (attrs) {
+			attrs = Marbles.Utils.extend({
+				id: 'new',
+				entity: this.entity,
+				mentions: [{
+					post: this.id
+				}].concat(this.mentions || []),
+				refs: [{
+					post: this.id
+				}]
+			}, attrs);
+			var message = Messenger.Models.Message.findOrInit(attrs, {fetch: false});
+			message.initConversation(this);
+			this.newMessage = message;
+			return message;
+		},
+
+		saveNewMessage: function (options) {
+			if (this.newMessage) {
+				this.newMessage.mentions = [{ post: this.id }].concat(this.mentions || []);
+				this.newMessage.refs = [{ post: this.id }];
+
+				this.newMessage.save({
+					success: function (res, xhr) {
+						this.newMessage = null;
+						options.success(res, xhr);
+					}.bind(this),
+
+					failure: function (res, xhr) {
+						options.failure(res, xhr);
+					},
+
+					complete: function (res, xhr) {
+						options.complete(res, xhr);
+					}
+				});
+				return;
+			}
+		},
+
+		save: function (options) {
+			var data = this.toJSON();
+			var callback = {
+				success: function (res, xhr) {
+					this.parseAttributes(res.post);
+					this.saveNewMessage({
+						success: function () {
+							if (typeof options.success === 'function') {
+								options.success(res, xhr);
+							}
+						},
+
+						failure: function (res, xhr) {
+							if (typeof options.failure === 'function') {
+								options.failure(res, xhr);
+							}
+						},
+
+						complete: function () {
+							if (typeof options.complete === 'function') {
+								options.complete(res, xhr);
+							}
+						}
+					});
+				}.bind(this),
+
+				failure: function (res, xhr) {
+					if (typeof options.failure === 'function') {
+						options.failure(res, xhr);
+					}
+				},
+
+				complete: function (res, xhr) {
+					if (typeof options.complete === 'function') {
+						options.complete(res, xhr);
+					}
+				}
+			};
+			if (this.id === 'new') {
+				delete data.id;
+				Messenger.client.createPost(data, {
+					callback: callback
+				});
+			} else {
+				data.version = {
+					parents: [{
+						version: data.version.id
+					}]
+				};
+				Messenger.client.updatePost(data, {
+					callback: callback
+				});
+			}
 		},
 
 		toJSON: function () {
 			var attrs = {},
 					keys = [
 						'id',
+						'type',
 						'entity',
 						'version',
 						'mentions',
