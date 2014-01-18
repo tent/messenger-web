2836089d87d3985c76f49b9544783d43c017a90c
Jesse Stuart <jesse@jessestuart.ca>
2014-01-17 22:14:54 -0500

    Add infinite scroll to conversations view

--- src/javascripts/views/conversations.js.jsx
+++ src/javascripts/views/conversations.js.jsx
@@ -5,7 +5,8 @@ Messenger.Views.Conversations = React.createClass({
 
 	getInitialState: function () {
 		return {
-			models: []
+			models: [],
+			lastPage: true
 		};
 	},
 
@@ -39,13 +40,24 @@ Messenger.Views.Conversations = React.createClass({
 	},
 
 	handleChangeConversations: function () {
-		this.setState({models: this.props.conversations.models()});
+		this.setState({
+			models: this.props.conversations.models(),
+			lastPage: !this.props.conversations.pages.next
+		});
+	},
+
+	loadNextPage: function () {
+		var res = this.props.conversations.fetchNext({ append: true });
+		if (res === false) {
+			this.setState({ lastPage: true });
+		}
 	},
 
 	render: function () {
 		var TruncatedMessage = Messenger.Views.TruncatedMessage,
 				RelativeTimestamp = Boiler.Views.RelativeTimestamp,
-				ConversationListItem = Messenger.Views.ConversationListItem;
+				ConversationListItem = Messenger.Views.ConversationListItem,
+				InfiniteScroll = React.addons.InfiniteScroll;
 		var items = [];
 		var models = this.state.models;
 		var conversation, messageNode, latestMessage;
@@ -63,9 +75,17 @@ Messenger.Views.Conversations = React.createClass({
 		}
 
 		return (
-			<ul className='unstyled conversations'>
-				{items}
-			</ul>
+			<div>
+				<ul className='unstyled conversations'>
+					{items}
+				</ul>
+
+				<InfiniteScroll
+					loadMore={this.loadNextPage}
+					hasMore={!this.state.lastPage}
+					loader={<div>Loading...</div>}
+					threshold={250} />
+			</div>
 		);
 	}
 });
