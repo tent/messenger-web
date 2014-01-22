40b368e5b9e5b881be0ac07e71f73c3eb62855e3
Jesse Stuart <jesse@jessestuart.ca>
2014-01-21 21:24:52 -0500

    Add name/avatar to messages

--- src/javascripts/views/message.js.jsx
+++ src/javascripts/views/message.js.jsx
@@ -4,21 +4,58 @@ Messenger.Views.Message = React.createClass({
 	displayName: 'Messenger.Views.Message',
 
 	getInitialState: function () {
-		return {};
+		return {
+			entity: null,
+			name: null,
+			avatarURL: null
+		};
+	},
+
+	componentWillMount: function () {
+		this.setState({
+			entity: this.props.message.get('entity'),
+			name: this.props.message.get('entity').replace(/^https?:\/\//, ''),
+			avatarURL: null
+		});
+
+		var component = this;
+		TentContacts.find(this.props.message.get('entity'), function (profile) {
+			var avatarURL = null;
+			if (profile.avatarDigest) {
+				avatarURL = Messenger.client.getNamedURL('attachment', [{
+					entity: profile.entity,
+					digest: profile.avatarDigest
+				}]);
+			} else {
+				// TODO: add sigil support
+			}
+
+			component.setState({
+				entity: profile.entity,
+				name: profile.name,
+				avatarURL: avatarURL
+			});
+		});
 	},
 
 	render: function () {
 		var RelativeTimestamp = Boiler.Views.RelativeTimestamp;
 		var message = this.props.message;
 
+		var avatar = '';
+		if (this.state.avatarURL) {
+			avatar = <img src={this.state.avatarURL} className='pull-left avatar-medium' />;
+		}
+
 		return (
 			<div>
+				{avatar}
 				<div className='pull-right'>
 					<small>
 						<RelativeTimestamp milliseconds={message.published_at} />
 					</small>
 				</div>
-				<h3>{message.get('entity')}</h3>
+				<h3 title={this.state.entity}>{this.state.name}</h3>
 				<p>{message.get('content.text') || ''}</p>
 			</div>
 		);
