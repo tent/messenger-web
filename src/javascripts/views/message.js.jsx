62c401527dc2c5949baf0581e2ddad49f421c2b9
Jesse Stuart <jesse@jessestuart.ca>
2014-01-29 15:44:41 -0500

    Add delete button to messages

--- src/javascripts/views/message.js.jsx
+++ src/javascripts/views/message.js.jsx
@@ -3,14 +3,70 @@
 Messenger.Views.Message = React.createClass({
 	displayName: 'Messenger.Views.Message',
 
+	getInitialState: function () {
+		return {
+			active: false,
+			deleting: false,
+			deleteFailed: false
+		};
+	},
+
+	handleDeleteClick: function (e) {
+		e.preventDefault();
+
+		if (confirm("Delete message?")) {
+			var message = this.props.message;
+
+			this.setState({
+				deleting: true
+			});
+
+			message.performDelete({
+				failure: function (res, xhr) {
+					this.setState({
+						deleting: false,
+						deleteFailed: true
+					});
+
+					setTimeout(function () {
+						throw Error(this.constructor.displayName +": failed to delete Message("+ JSON.stringify(message.entity) +", "+ JSON.stringify(message.id) +"): "+ xhr.status +" "+ JSON.stringify(res));
+					}.bind(this), 0);
+				}.bind(this)
+			});
+		}
+
+		return false;
+	},
+
+	handleMouseEnter: function () {
+		this.setState({
+			active: true
+		});
+	},
+
+	handleMouseLeave: function () {
+		this.setState({
+			active: false
+		});
+	},
+
 	render: function () {
 		var RelativeTimestamp = Boiler.Views.RelativeTimestamp,
 				ContactAvatar = Messenger.Views.ContactAvatar,
 				ContactName = Messenger.Views.ContactName;
 		var message = this.props.message;
 
+		var deleteBtn;
+		var deleteBtn;
+		if (this.state.active && !this.state.deleting && !this.state.deleteFailed && message.entity === Messenger.current_entity) {
+			deleteBtn = <button className='btn btn-danger' title='Delete conversation' onClick={this.handleDeleteClick}>Delete</button>;
+		} else {
+			deleteBtn = '';
+		}
+
 		return (
-			<div>
+			<div className='message clearfix' onClick={this.handleClick} onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave} className={'clearfix'+ (this.state.deleting ? ' deleting' : '') + (this.state.deleteFailed ? ' delete-failed' : '')}>
+
 				<span className='pull-left avatar-container'>
 					<ContactAvatar entity={this.props.message.get('entity')} className='avatar-medium' />
 				</span>
@@ -22,6 +78,11 @@ Messenger.Views.Message = React.createClass({
 				<h3>
 					<ContactName entity={this.props.message.get('entity')} />
 				</h3>
+
+				<div className='pull-right'>
+					{deleteBtn}
+				</div>
+
 				<p>{message.get('content.text') || ''}</p>
 			</div>
 		);
